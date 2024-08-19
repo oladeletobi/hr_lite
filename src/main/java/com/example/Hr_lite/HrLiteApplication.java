916dff9c01ca209bcab3bed5ee33fc7f6bc96d3d
oladeletobi <tobi@bepeerless.co>
2024-08-19 08:10:45 +0100

    setup

---
+++ src/main/java/com/example/Hr_lite/HrLiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hr_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hr_lite/HrLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hr_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
